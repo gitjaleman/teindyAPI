a882e3db5e0715538a8306053e4617f19fafe166
jaleman <javiersettex@gmail.com>
2022-03-06 22:39:54 -0500

    base Project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
+const device = require('express-device');
 const app = express();
+//const useRoutes = require('./routes/index');
 
 //configuraciones
 app.set('port',process.env.PORT || 3000);
@@ -10,10 +12,18 @@ app.set('port',process.env.PORT || 3000);
 //middleware  o configuraciones  intermedias
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(device.capture());
 
+
+//rutas
+app.get('/', (req, res)=>{
+  console.log('dispositivo ' +req.device.type);
+//console.log(req.device);
+ res.send('respuesta del servidor');
+})
 
 
 //iniciar servidor
 app.listen(app.get('port'), ()=>{
-  console.log('servidor corriendo en el puerto', app.get('port'));
+  console.log('servidor  en el puerto', app.get('port'));
 });
